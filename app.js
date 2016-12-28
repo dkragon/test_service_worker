a79dd9c1aeed883912287da9c84c7db38518bb0a
dkragon <237198484@qq.com>
2016-12-28 09:31:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // register service worker
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
+  navigator.serviceWorker.register('/test_service_worker/sw.js', { scope: '/test_service_worker/' }).then(function(reg) {
     
     if(reg.installing) {
       console.log('Service worker installing');
